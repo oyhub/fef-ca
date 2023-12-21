441f78c6548d9e624511fb4440e69f773a5a9eb4
oyhub <oyvind@hunstad.org>
2023-12-21 14:49:31 +0100

    header and footer layout fix

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -5,21 +5,21 @@ export function Header() {
 return (
   <header>
     <div className="text-xs font-heading uppercase w-screen bg-primary text-white p-2">
-      <div className="container mx-auto flex justify-end gap-8">
+      <div className="container flex justify-end gap-8">
         <a href="#">Contact</a>
         <a href="#">About us</a>
         <a href="#">+47 99 99 99 99</a>
       </div>
     </div>
     <div>
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container flex justify-between items-center">
         <Link to="/" className="uppercase text-3xl m-2">
-          <span className="text-primary">linnus</span> <span className="bg-primary text-white px-2">fritid</span>
+          <span className="text-primary border-primary border-2 px-2 xs:border-0">l<span className="hidden xs:inline">innus</span></span><span className="bg-primary border-primary border-2 text-white px-2">f<span  className="hidden xs:inline">ritid</span></span>
         </Link>
         <div className="w-1/2">
           <input aria-label="Search product" type="search" placeholder="Search" className="p-2 w-full max-w-xl shadow border-1 rounded"/>
         </div>
-        <button className="p-1 text-gray-500 hover:text-primary">
+        <button className="p-1 mr-1 text-gray-500 hover:text-primary">
           <ShoppingCartRounded />
         </button>
       </div>
