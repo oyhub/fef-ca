32418db6e2ef9a58f6189422b6dadf1a27e8b161
oyhub <oyvind@hunstad.org>
2024-01-08 20:35:40 +0100

    page titles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import FeatureProducts from '../components/api/featureProducts/FeatureProducts.tsx';
 import ShowProducts from '../components/api/showProducts/ShowProducts.tsx';
+import useDocumentTitle from '../components/hooks/useDocumentTitle.tsx';
 
 export function Home() {
+  useDocumentTitle('Linnus Fritid')
   return (
     <>
       <FeatureProducts />
